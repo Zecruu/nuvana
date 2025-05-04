8bc1ad1690aa8a1c50feecf1519090ed16c4a3e1
Your Name <example@example.com>
2025-05-04 14:44:16 -0400

    Add Google Analytics tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,17 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-75Z7QBM99R"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-75Z7QBM99R');
+          `
+        }} />
       </Head>
       <body>
         <Main />
